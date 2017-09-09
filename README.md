58e0ed2b6967d8b05b4977f2d4dfa7b07d3cddcd
baijunjie <625603381@qq.com>
2017-09-10 01:40:16 +0900

    v1.0.0

---
+++ README.md
@@ -0,0 +1,75 @@
+# react-native-input-scroll-view
+Mainly to solve the following problems：
+
+- When the keyboard pops up, the `TextInput` will automatically adjust to the top of the keyboard.
+- When the multiline `TextInput` create new line, the new line will automatically adjust to the top of the keyboard.
+- When the finger is placed above the `TextInput` and scroll the view, finger lift, the input doesn't get the focus.
+
+All of the above problems are in IOS.
+
+
+
+## Installation
+
+npm
+
+```shell
+$ npm install react-native-input-scroll-view --save
+```
+
+yarn
+
+```shell
+$ yarn add react-native-input-scroll-view
+```
+
+
+
+##Usage
+
+Very easy
+
+```jsx
+import InputScrollView from 'InputScrollView';
+...
+render() {
+    return (
+        <InputScrollView>
+            <TextInput/>
+      	</InputScrollView>
+    );
+}
+```
+
+
+
+## Props
+
+####props.lock
+
+Make the scrollview no longer follow the keyboard adjustment, default is `false`. Sometimes you might need it.
+
+To monitor multiline `TextInput`, the `ScrollView`'s `onContentSizeChange` event is used. But that brings with it a side effect, when the `ContentSize` of `ScrollView` changes is not due to multiline text input, and at the same time the keyboard is hidden, may result in a `ScrollView` error correction, set it to `true`, might solve the problem.
+
+#### props.topOffset
+
+If you find out that the keyboard pops up, the top of the ScrollView shows that the location is not normal, you just need to set it to `navigatorHeight`.
+
+####props.bottomOffset
+
+If you find that the keyboard pops up, the bottom of the ScrollView is not normal, you just need to set it to `tabBarHeight`.
+
+
+
+## ENV
+
+```
+"react": "16.0.0-alpha.12"
+"react-native": "0.48.0-rc.1"
+```
+
+
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,119 @@
+/**
+ * @providesModule InputScrollView
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+    View,
+    ScrollView,
+    Dimensions,
+} from 'react-native';
+import TextInputState from 'TextInputState';
+
+export default class extends Component {
+    static propTypes = {
+        lock: PropTypes.bool,
+        topOffset: PropTypes.number,
+        bottomOffset: PropTypes.number,
+    };
+
+    static defaultProps = {
+        lock: false,
+        topOffset: 0,
+        bottomOffset: 0,
+    };
+
+    componentWillMount() {
+        this._keyboardShow = false;
+        this._curFocusTarget = null;
+    }
+
+    render() {
+        const { children, otherProps } = this.props;
+        return (
+            <ScrollView ref="scrollView"
+                        keyboardShouldPersistTaps="handled"
+                        onFocus={this._onFocus}
+                        onBlur={this._onBlur}
+                        onContentSizeChange={this._scrollToKeyboard}
+                        onKeyboardDidShow={this._onKeyboardDidShow}
+                        onKeyboardWillHide={this._onKeyboardWillHide} {...otherProps}>
+                <View onStartShouldSetResponderCapture={this._onStartShouldSetResponderCapture}>
+                    {children}
+                </View>
+            </ScrollView>
+        );
+    }
+
+    _setScrollViewContentBottomInset(bottomInset) {
+        const scrollView = this.refs.scrollView;
+        scrollView &&
+        scrollView.setNativeProps({
+            contentInset : {
+                left: 0,
+                right: 0,
+                top: scrollView.props.automaticallyAdjustContentInsets === false ? 0 : -this.props.topOffset,
+                bottom: bottomInset - this.props.bottomOffset
+            }
+        });
+    }
+
+    _scrollToEnd() {
+        const scrollView = this.refs.scrollView;
+        scrollView &&
+        scrollView.scrollToEnd({animated: true});
+    }
+
+    _onKeyboardDidShow = (event) => {
+        const keyboardHeight = Dimensions.get('window').height - event.endCoordinates.screenY;
+        this._setScrollViewContentBottomInset(keyboardHeight);
+
+        if (this._keyboardShow) return;
+        this._keyboardShow = true;
+        // 如果 keyboardStatus 为 true，则说明在 onFocus 事件已经处理过了，这里无需在进行处理
+        this._scrollToKeyboard();
+    };
+
+    _onKeyboardWillHide = () => {
+        this._keyboardShow = false;
+        this._setScrollViewContentBottomInset(this.props.bottomOffset);
+        this._scrollToEnd();
+    };
+
+    _scrollToKeyboard = () => {
+        // 当 ScrollView 的 ContentSize 不是因为多行文本输入而发生变化时，会导致这里错误的执行
+        // 因此需要使用一个属性将其锁住
+        if (this.props.lock || !this._curFocusTarget || !this._keyboardShow) return;
+        const scrollView = this.refs.scrollView;
+        scrollView &&
+        scrollView.scrollResponderScrollNativeHandleToKeyboard(this._curFocusTarget, 80, true);
+    };
+
+    _onFocus = ({nativeEvent:event}) => {
+        this._curFocusTarget = event.target;
+        this._scrollToKeyboard();
+    };
+
+    _onBlur = () => {
+        this._curFocusTarget = null;
+    };
+
+    // 这个方法是为了防止 ScrollView 在滑动结束后触发 TextInput 的 focus 事件
+    _onStartShouldSetResponderCapture = ({...event}) => {
+        if (this._curFocusTarget) {
+            const uiViewClassName = event._targetInst.viewConfig.uiViewClassName;
+            if (uiViewClassName !== 'RCTTextField' && uiViewClassName !== 'RCTTextView') {
+                TextInputState.focusTextInput(this._curFocusTarget);
+                TextInputState.blurTextInput(this._curFocusTarget);
+            }
+            return false;
+        }
+
+        // 当返回 true 时
+        // 被激活的 TextInput 无法使用 Keyboard.dismiss() 来收起键盘
+        // TextInputState.currentlyFocusedField() 也无法获取当前焦点ID
+        // 因此任然需要依靠 _curFocusTarget 变量来记录当前焦点
+        return true;
+    };
+}
